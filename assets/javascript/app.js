48887413d894c3a813b50c21d71122620ea0d6cc
mohamed ahmed <ahmedmohamed742@gmail.com>
2019-10-12 19:53:03 -0500

    HW

---
+++ assets/javascript/app.js
@@ -0,0 +1,210 @@
+$(document).ready(function() {
+	var index = 0;
+	var countdownTimer = {
+		time : 15,
+		reset: function() {
+			this.time = 15;
+			$('.timer').html('<h3>' + this.time + ' seconds remaining</h3>');
+		},
+		start: function() {
+			counter = setInterval(countdownTimer.count, 1000);	
+		},
+		stop: function() {
+			clearInterval(counter);
+		},
+		count: function() {
+				countdownTimer.time--;
+				console.log(countdownTimer.time);
+//				$('.timer').html(countdownTimer.time);
+			if (countdownTimer.time >= 0) {
+				$('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
+			}
+			else {
+				index++;
+				answerWrong();
+				countdownTimer.reset();
+				if (index < questionArray.length) {
+					loadQuestion(index);
+				} else {
+					$(".answerchoice").hide();
+					showScore();
+				}
+			}
+		}
+	};
+
+var correct = 0;
+var wrong = 0;
+var q1 = {
+	question : 'Who is the best Basketball player right now?',
+	possibleAnswers : ['A. Kyrie Irving',
+				 'B. Lebron James',
+				 'C. Paul Geroge',
+				 'D. Kevin Durant'],
+	flags : [false, false, true, false],
+	answer : 'C. Paul George'
+};
+
+var q2 = {
+	question: 'what NFL team won the 2019 SuperBowl?',
+	possibleAnswers: ['A. Vikings',
+				 'B. Patriots',
+				 'C. Packers',
+				 'D. Rams'],
+	flags : [false, true, false, false],
+	answer : 'B. Patriots'
+};
+
+var q3 = {
+	question : 'what country will host the 2018 world cup?',
+	possibleAnswers : ['A. qatar',
+				 'B. russia',
+				 'C. egypt',
+				 'D. argentina'],
+	flags : [false, true, false, false],
+	answer : 'B. russia'
+};
+
+var q4 = {
+	question : 'how many championsip title does Kobe Bryant have?',
+	possibleAnswers : ['A. 5',
+				 'B. 0',
+				 'C. 2',
+				 'D. 7'],
+	flags : [true, false, false, false],
+	answer : 'A. 5'
+};
+
+var q5 = {
+	question : 'What country was welcomed into the World Showcase at EPCOT Center in 1984?',
+	possibleAnswers : ['A. Norway',
+				 'B. Morocco',
+				 'C. France',
+				 'D. Japan'],
+	flags : [false, true, false, false],
+	answer : 'B. Morocco'
+};
+
+
+
+
+
+
+
+
+
+var questionArray = [q1, q2, q3, q4, q5,];
+
+function loadQuestion(questionSelection) {
+	console.log(questionSelection);
+	countdownTimer.reset();
+  $(".question").html("<h3>" + questionArray[questionSelection].question + "</h3>");
+  $("#buttonA").text(questionArray[questionSelection].possibleAnswers[0]).show();
+  $("#buttonB").text(questionArray[questionSelection].possibleAnswers[1]).show();
+  $("#buttonC").text(questionArray[questionSelection].possibleAnswers[2]).show();
+  $("#buttonD").text(questionArray[questionSelection].possibleAnswers[3]).show();
+//  getAnswer();  
+//  nextQuestion(index);
+}
+
+//function nextQuestion() {
+//	index = index++;
+//	console.log(index);
+//}
+
+function setup() {
+	index = 0;
+	$('.question').append('<button id="startButton">Start</button>');
+	$('#startButton').on('click', function() {
+		$(this).hide();
+		countdownTimer.start();
+	 	loadQuestion(index);
+	});
+}		
+
+function getAnswer() {
+
+//  nextQuestion();
+	$('.answerchoice').on('click', function() {
+	  console.log('alert', index);
+		index++;
+		console.log('click', index);
+		$(".question").text('');
+		$("#buttonA").text('');
+		$("#buttonB").text('');
+		$("#buttonC").text('');
+		$("#buttonD").text('');
+		loadQuestion();
+	})
+}
+
+function answerCorrect() {
+	correct++;
+	alert("Correct!");
+	console.log("correct");
+}
+
+function answerWrong() {
+	wrong++;
+	alert("Incorrect!");
+	console.log("wrong");
+}
+
+function showScore() {
+	$('.question').empty();
+	$('.question').append("<h2><p>" + correct + " correct</p></h2>");
+	$('.question').append("<h2><p>" + wrong + " incorrect</p></h2>");
+	countdownTimer.stop();
+	$('.timer').empty();
+
+}
+setup();
+$('.answerchoice').on('click', function() {
+ console.log($(this));
+ if(this.id == 'buttonA') {
+ 	var answerChosen = 'A';
+ } else if(this.id == 'buttonB') {
+ 	answerChosen = 'B';
+ } else if (this.id == 'buttonC') {
+ 	answerChosen = 'C';
+ } else if (this.id == 'buttonD') {
+ 	answerChosen = 'D';
+ } 
+ if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'A') {
+ 	answerWrong();
+ }
+ if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'B') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'C') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'D') {
+ 	answerWrong();
+ }
+
+ $(".question").text('');
+ $("#buttonA").text('');
+ $("#buttonB").text('');
+ $("#buttonC").text('');
+ $("#buttonD").text('');
+ index++;
+ if (index < questionArray.length) {
+ 	loadQuestion(index);
+ } else {
+ 	$(".answerchoice").hide();
+ 	showScore();
+ }
+});
+
+
+//	$('#start').click(countdownTimer.start);
+});
